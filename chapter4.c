e906b110cc277cc926753b9c507fb99bef0386b7
suchitramunyal <suchitramunyal27@gmail.com>
2026-08-04 23:21:55 +0530

    Add files via upload

---
+++ chapter4.c
@@ -0,0 +1,25 @@
+// (Chapter4) Loop Control Statements
+
+// 1. Syntax of 3 Loops
+# include <stdio.h>
+int main () {
+//for loop
+for(int i=1; i<=100; i++) {
+printf("%d\n", i);
+}
+
+//while loop
+int i=1;
+while(i<=100) {
+printf("%d\n", i);
+i++;
+}
+
+//do while loop
+i = 1;
+do {
+    printf("%d\n", i);
+i++;
+} while(i<=100);
+return 0;
+}
